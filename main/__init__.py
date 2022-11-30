d96c91d860dddbf8881caa6c8f65f1ce1f6f67d3
savidnet <32696531+savidnet@users.noreply.github.com>
2022-11-30 15:04:14 +0700

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = 20462802
-API_HASH = dd98d5b8a57cf65d450b746a9c9052af
-BOT_TOKEN = 5625785348:AAEqTOnCUFEVjDqg0ws4XghXH9odoHo5t6M
-SESSION = BQCspFYVybNYwVSsV033CINE2f85Ng49rYnBKjw-QwQ3HZQ1ZImL7Tqrvwfa4VSg4roRoiJp-1V1DPiJP1MSQs6Hfq4i5Kam8wXtJdFl8TxhSk28UqkCIf76IdWAVpmBG03GMstK97RJ67gUfoqip662r54vsz-jaj1eiw8uftHrTS4yEpNJ6zWuW155FIYhoCpl1Ic0KwPg_YYB5lD3lx6yrPHVmwqJMM29a2ocX114p8jLS1dxW6r5mR8q5H2fNgSeihtCLMmFq4hXGDVOGI9pABz2XP52lnKokkHZD73LCYqUTM-x5s8cprFJCSBZs9v3QFNRlXgPV_wevp9F-zPlAAAAAUq9VukA
-FORCESUB = -1001468992261
-AUTH = 5548889833
+API_ID = config("20462802", default=None, cast=int)
+API_HASH = config("dd98d5b8a57cf65d450b746a9c9052af", default=None)
+BOT_TOKEN = config("5625785348:AAEqTOnCUFEVjDqg0ws4XghXH9odoHo5t6M", default=None)
+SESSION = config("BQCspFYVybNYwVSsV033CINE2f85Ng49rYnBKjw-QwQ3HZQ1ZImL7Tqrvwfa4VSg4roRoiJp-1V1DPiJP1MSQs6Hfq4i5Kam8wXtJdFl8TxhSk28UqkCIf76IdWAVpmBG03GMstK97RJ67gUfoqip662r54vsz-jaj1eiw8uftHrTS4yEpNJ6zWuW155FIYhoCpl1Ic0KwPg_YYB5lD3lx6yrPHVmwqJMM29a2ocX114p8jLS1dxW6r5mR8q5H2fNgSeihtCLMmFq4hXGDVOGI9pABz2XP52lnKokkHZD73LCYqUTM-x5s8cprFJCSBZs9v3QFNRlXgPV_wevp9F-zPlAAAAAUq9VukA", default=None)
+FORCESUB = config("-1001468992261", default=None)
+AUTH = config("5548889833", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
